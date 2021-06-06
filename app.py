f22734c47a7850f0b32ffc36265e7b6c233e5913
Somethea Siek <sometheasiekswx@gmail.com>
2021-06-06 16:55:41 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,93 @@
 from signal import signal, SIGINT
 from sys import exit
-
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+from time import sleep
 from flask import Flask
+
+GPIO.setmode(GPIO.BCM)
+
+# Motion
+
+motion_pin = 16
+led_pin = 21
+no_motion_count = 0
+
+GPIO.setup(motion_pin, GPIO.IN)
+GPIO.setup(led_pin, GPIO.OUT)
+
+
+def handle_motion(no_motion_count):
+    if GPIO.input(motion_pin):
+        print("Motion Detected!")
+        GPIO.output(led_pin, True)
+        sleep(4)
+        GPIO.output(led_pin, False)
+        return 0
+    else:
+        return no_motion_count + 1
+
+
+# Temperature + Humidity
+
+temperature_humidity_sensor = Adafruit_DHT.DHT11
+gpio_pin = 4
+
+
+def handle_temperature():
+    humidity, temperature = Adafruit_DHT.read_retry(
+        temperature_humidity_sensor, gpio_pin)
+    if humidity is not None and temperature is not None:
+        print(
+            'Temperature = {0:0.1f}*C  Humidity = {1:0.1f}%'.format(temperature, humidity))
+        return temperature
+    else:
+        print('Failed to read Temperature/Humidity')
+
+
+# Run Program
+
+print("Sensor initializing . . .")
+sleep(5)
 
 app = Flask(__name__)
 
+no_motion_count = 0
+desired_temperature = 28
+desired_temperature_margin = 2
+
+
+@app.route("/temperature")
+def get_temperature():
+    humidity, temperature = Adafruit_DHT.read_retry(
+        temperature_humidity_sensor, gpio_pin)
+    if humidity is not None and temperature is not None:
+        print(
+            'Temperature = {0:0.1f}*C  Humidity = {1:0.1f}%'.format(temperature, humidity))
+        return temperature
+    else:
+        print('Failed to read Temperature/Humidity')
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    # while True:
+    #     temperature = handle_temperature()
+    #     no_motion_count = handle_motion(no_motion_count)
+    #
+    #     if no_motion_count >= 20:
+    #         print(f"No Human Detected.")
+    #     elif temperature > desired_temperature + desired_temperature_margin:
+    #         print(f"Temperature Too High")
+    #     elif temperature < desired_temperature - desired_temperature_margin:
+    #         print(f"Temperature Too Low")
+    #     else:
+    #         print(f"Temperature Just Right")
+    #
+    #     print(f"No Motion Count: {no_motion_count}")
+    #
+    #     sleep(0.25)
+    return "NOTHING HERE"
 
 
 def cleanup(signal, frame):
